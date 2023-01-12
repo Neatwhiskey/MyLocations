0f63a09a3524d88cf3b2b222a740a8badd3145e9
Neatwhiskeyy <neatwhiskey24@gmail.com>
2023-01-12 23:41:13 +0100

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Jamaaldeen Opasina on 12/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
